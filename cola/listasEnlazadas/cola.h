2e81ef169e4ae6b74b31c78f7a8d90abdf3866e9
xuzengmao <xmzx@protonmail.com>
2018-04-23 00:48:18 +0200

    Añadido: cola/listasEnlazadas

---
+++ cola/listasEnlazadas/cola.h
@@ -0,0 +1,35 @@
+
+
+/*****************************************************************************/
+/*   Autor: Xuzeng Mao                                                       */
+/*   Correo para cualquier duda o sugerencia: xuzengmao@usal.es              */
+/*   GitHub: https://github.com/xuzengmao                                    */
+/*   Fuente: cola.c cola.h                                                   */
+/*****************************************************************************/
+
+
+#ifndef __COLA_H
+#define __COLA_H
+
+typedef int tipoElemento;
+
+#ifndef __TIPOCELDA
+#define __TIPOCELDA
+
+typedef struct tipoCelda { 
+	tipoElemento elemento; 
+	struct tipoCelda * sig; 
+	} tipoCelda;
+	
+#endif
+
+typedef struct {
+        tipoCelda *frente, *fondo;
+} Cola;
+
+int colaCreaVacia(Cola *c);
+int colaVacia(Cola *c);
+int colaInserta(Cola *c, tipoElemento elemento);
+tipoElemento colaSuprime(Cola *c);
+
+#endif
