1f9d9cfcdbe6179e41cdec5d8f5b451013a0a1c1
xuzengmao <xmzx@protonmail.com>
2018-04-23 18:33:29 +0200

    Modificado: añadido nuevas funciones de arbol

--- arbol/memoriaDispersa/arbol.c
+++ arbol/memoriaDispersa/arbol.c
@@ -91,3 +91,167 @@ void amplitud(Arbol raiz) {
 	}
 }
 
+
+int altura(Arbol raiz) {
+	int izq, der;
+
+	if (raiz == NULL)
+		return -1;
+
+	izq = altura(raiz->izq);
+	der = altura(raiz->der);
+
+	if (izq < der)
+		return 1 + der;
+
+	return 1 + izq;
+}
+
+
+int numNodos(Arbol raiz) {
+	if (raiz == NULL)
+		return 0;
+
+	return 1 + numNodos(raiz->izq) + numNodos(raiz->der);
+}
+
+
+int sustituye(Arbol raiz, tipoInfo x, tipoInfo y) {
+	int cont = 0;
+
+	if (raiz == NULL)
+		return 0;
+
+	if (raiz->info == x) {
+		raiz->info = y;
+		cont++;
+	}
+
+	return cont + sustituye(raiz->izq, x, y) + sustituye(raiz->der, x, y);
+}
+
+
+int numNodosHoja(Arbol raiz) {
+	if (raiz == NULL)
+		return 0;
+
+	if (raiz->izq == NULL && raiz->der == NULL)
+		return 1;
+
+	return numNodosHoja(raiz->izq) + numNodosHoja(raiz->der);
+}
+
+
+int numNodosInternos(Arbol raiz) {
+	if (raiz == NULL)
+		return 0;
+
+	if (raiz->izq == NULL && raiz->der == NULL)
+		return 0;
+
+	return 1 + numNodosInternos(raiz->izq) + numNodosInternos(raiz->der);
+}
+
+
+int numHijoUnico(Arbol raiz) {
+	int cont = 0;
+
+	if (raiz == NULL)
+		return 0;
+
+	if (raiz->izq != NULL && raiz->der == NULL ||
+	    raiz->izq == NULL && raiz->der != NULL)
+	    cont++;
+
+	return cont + numHijoUnico(raiz->izq) + numHijoUnico(raiz->der);
+}
+
+
+Arbol buscarMax(Arbol raiz) {
+	Arbol izq, der;
+
+	if (raiz == NULL)
+		return NULL;
+
+	izq = buscarMax(raiz->izq);
+	der = buscarMax(raiz->der);
+
+	if (izq != NULL && izq->info > raiz->info) {
+		if (der != NULL && der->info > izq->info)
+			return der;
+
+		return izq;
+	}
+
+	if (der != NULL && der->info > raiz->info)
+		return der;
+
+	return raiz;
+}
+
+
+Arbol buscarMin(Arbol raiz) {
+	Arbol izq, der;
+
+	if (raiz == NULL)
+		return NULL;
+
+	izq = buscarMin(raiz->izq);
+	der = buscarMin(raiz->der);
+
+	if (izq != NULL && izq->info < raiz->info) {
+		if (der != NULL && der->info < izq->info)
+			return der;
+
+		return izq;
+	}
+
+	if (der != NULL && der->info < raiz->info)
+		return der;
+
+	return raiz;
+}
+
+
+int similares(Arbol r1, Arbol r2) {
+	if (r1 == NULL && r2 == NULL)
+		return 1;
+
+	if (r1 == NULL && r2 != NULL || r1 != NULL && r2 == NULL)
+		return 0;
+
+	if (!similares(r1->izq, r2->izq))
+		return 0;
+
+	return similares(r1->der, r2->der);
+}
+
+
+int equivalentes(Arbol r1, Arbol r2) {
+	if (r1 == NULL && r2 == NULL)
+		return 1;
+
+	if (r1 == NULL && r2 != NULL || r1 != NULL && r2 == NULL)
+		return 0;
+
+	if (r1->info != r2->info)
+		return 0;
+
+	if (!equivalentes(r1->izq, r2->izq))
+		return 0;
+
+	return equivalentes(r1->der, r2->der);
+}
+
+
+Arbol especular(Arbol raiz) {
+	Arbol temp;
+
+	if (raiz == NULL)
+		return NULL;
+
+	temp = creaNodo(raiz->info);
+	temp->der = especular(raiz->izq);
+	temp->izq = especular(raiz->der);
+	return temp;
+}
--- arbol/memoriaDispersa/arbol.h
+++ arbol/memoriaDispersa/arbol.h
@@ -32,5 +32,16 @@ void enOrden(Arbol raiz);
 void postOrden(Arbol raiz);
 void amplitud(Arbol raiz);
 
+int altura(Arbol raiz);
+int numNodos(Arbol raiz);
+int sustituye(Arbol raiz, tipoInfo x, tipoInfo y);
+int numNodosHoja(Arbol raiz);
+int numNodosInternos(Arbol raiz);
+int numHijoUnico(Arbol raiz);
+Arbol buscarMax(Arbol raiz);
+Arbol buscarMin(Arbol raiz);
+int similares(Arbol r1, Arbol r2);
+int equivalentes(Arbol r1, Arbol r2);
+Arbol especular(Arbol raiz);
 
 #endif
