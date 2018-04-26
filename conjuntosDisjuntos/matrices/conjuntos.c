0ab84b86fd3c189468f8a94bdffbd7f60f704911
xuzengmao <xmzx@protonmail.com>
2018-04-26 21:50:57 +0200

    Añadido: conjuntosDisjuntos

---
+++ conjuntosDisjuntos/matrices/conjuntos.c
@@ -0,0 +1,17 @@
+
+
+/****************************************************************************/
+/*   Autor: Xuzeng Mao                                                      */
+/*   Correo para cualquier duda o sugerencia: xuzengmao@usal.es             */
+/*   GitHub: https://github.com/xuzengmao                                   */
+/*   Fuente principal:                                                      */
+/*       - Conjuntos: conjuntos.c conjuntos.h                               */
+/****************************************************************************/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "conjuntos.h"
+
+
---
+++ conjuntosDisjuntos/matrices/conjuntos.h
@@ -0,0 +1,28 @@
+
+
+/****************************************************************************/
+/*   Autor: Xuzeng Mao                                                      */
+/*   Correo para cualquier duda o sugerencia: xuzengmao@usal.es             */
+/*   GitHub: https://github.com/xuzengmao                                   */
+/*   Fuente principal:                                                      */
+/*       - Conjuntos: conjuntos.c conjuntos.h                               */
+/****************************************************************************/
+
+
+#ifndef __CONJUNTOS_H
+#define __CONJUNTOS_H
+
+#define MAXIMO 16
+
+typedef int particion[MAXIMO];
+typedef int tipoConjunto;
+typedef int tipoElemento;
+
+void crea(particion C);
+tipoElemento buscar(particion C, tipoElemento x);
+void unir(particion C, tipoElemento x, tipoElemento y);
+
+void verParticion(particion C);
+void verClaseEquivalencia(tipoElemento x, particion P);
+
+#endif
