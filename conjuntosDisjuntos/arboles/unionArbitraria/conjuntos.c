8bc273b368ff3b14232e741cc3d5ba5324b6f1ce
xuzengmao <xmzx@protonmail.com>
2018-04-27 19:58:29 +0200

    Añadido: conjuntosDisjuntos/arboles/unionArbitraria

---
+++ conjuntosDisjuntos/arboles/unionArbitraria/conjuntos.c
@@ -0,0 +1,63 @@
+
+
+/****************************************************************************/
+/*   Autor: Xuzeng Mao                                                      */
+/*   Correo para cualquier duda o sugerencia: xuzengmao@usal.es             */
+/*   GitHub: https://github.com/xuzengmao                                   */
+/*   Fuente principal:                                                      */
+/*       - Conjuntos: conjuntos.c conjuntos.h                               */
+/****************************************************************************/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "conjuntos.h"
+
+
+void crea(particion C) {
+	int i;
+
+	for (i = 0; i < MAXIMO; i++)
+		C[i] = i;
+}
+
+
+tipoElemento buscar(particion C, tipoElemento x) {
+	if (x < 0 || x >= MAXIMO)
+		return -1;
+
+	if (C[x] == x)
+		return x;
+
+	return buscar(C, C[x]);
+}
+
+
+void unir(particion C, tipoElemento x, tipoElemento y) {
+	if (x != -1 && y != -1)
+		C[y] = x;
+}
+
+
+void verParticion(particion C) {
+	int i;
+
+	printf("\n");
+
+	for (i = 0; i < MAXIMO; i++)
+		printf("|%2d", C[i]);
+
+	printf("| contenido vector\n");
+
+	for (i = 0; i < MAXIMO; i++)
+		printf("---");
+
+	printf("\n");
+
+	for (i = 0; i < MAXIMO; i++)
+		printf(" %2d", i);
+
+	printf("  indice vector\n\n");
+}
+
